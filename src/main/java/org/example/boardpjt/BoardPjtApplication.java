0ff7bd91fddc52f021486def91593f71d547c952
suhyungk <dook4966@gmail.com>
2024-04-06 13:08:30 +0900

    #6 - 스프링 부트 프로젝트 시작
    * 스프링 웹
    * 액츄에이터
    * 데브툴즈
    * 롬복

---
+++ src/main/java/org/example/boardpjt/BoardPjtApplication.java
@@ -0,0 +1,13 @@
+package org.example.boardpjt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardPjtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardPjtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/boardpjt/BoardPjtApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.boardpjt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardPjtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
